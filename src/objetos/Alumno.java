2be7077caa0f34bdac65f80f8e43eb8a73fd5113
Moyano <josemariamoyanomorales@cdpjosecabrera.es>
2024-11-19 12:58:50 +0100

    first commit

---
+++ src/objetos/Alumno.java
@@ -0,0 +1,17 @@
+package objetos;
+
+import java.util.Scanner;
+
+public class Alumno {
+
+    public Alumno(){
+        Scanner sc= new Scanner(System.in);
+
+        String alumno;
+        String nombre;
+        System.out.println("Ingrese el nombre del Alumno: ");
+        nombre = sc.nextLine();
+        
+
+    }
+}
---
+++ src/vista/Main.java
@@ -0,0 +1,11 @@
+package vista;
+
+import objetos.Alumno;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Alumno alumno = new Alumno();
+    }
+}
